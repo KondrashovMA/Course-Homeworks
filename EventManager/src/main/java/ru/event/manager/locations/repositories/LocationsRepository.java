1ae08ac681602f8a18a040c361a05da590691732
KondrashovMA <kondrashov.michael@mail.ru>
2025-09-29 14:37:23 +0300

    delete usless methods in jpa

--- EventManager/src/main/java/ru/event/manager/locations/repositories/LocationsRepository.java
+++ EventManager/src/main/java/ru/event/manager/locations/repositories/LocationsRepository.java
@@ -11,11 +11,6 @@ public interface LocationsRepository extends JpaRepository<LocationEntity, Long>
 
     @Query(value = """
             select * from locations
-            """, nativeQuery = true)
-    List<LocationEntity> getLocations();
-
-    @Query(value = """
-            select * from locations
             where (:name is null or locations.name = :name)
             and (:capacity is null or capacity >= :capacity)
             """, nativeQuery = true)
